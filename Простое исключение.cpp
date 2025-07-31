ad6c9d48ff1e4bb850fa4e1e14206c6c4c68c1d5
Klea Helga <elaginas27@gmail.com>
2025-07-31 20:38:18 +0300

    Commit 1

---
+++ Простое исключение.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+class bad_len : public exception {
+public:
+	const char* what() const noexcept override {
+		return "Вы ввели слова запретной длины! Досвидания.";
+	}
+};
+
+int function(string str, int forbid_len) {
+	int len = str.length();
+	if (len == forbid_len) { throw bad_len(); }
+	return len;
+}
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+	int forbid_len;
+	cout << "Введите запретную длинну: ";
+	cin >> forbid_len;
+
+	string word;
+	while (true) {
+		setlocale(LC_ALL, "Russian");
+		cout << "Введите слово: ";
+		cin >> word;
+
+		try {
+
+			int len = function(word, forbid_len);
+			cout << "Длина слова " << word << " равна " << len << endl;
+		}
+		catch (const bad_len& e) {
+			cout << e.what() << endl;
+			break;
+		}
+	}
+	return 0;
+}
